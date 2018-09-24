bd332c16eec024253f00ecba2f0e00024e743d1d
curiousclarissa <clarissa.jarem@gmail.com>
2018-09-23 20:24:24 -0700

    need to complete the preferences drawer

--- app/src/main/java/com/example/clarissajarem/newsappstage1/NewsActivity.java
+++ app/src/main/java/com/example/clarissajarem/newsappstage1/NewsActivity.java
@@ -4,9 +4,11 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,7 +24,7 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
 
     public static final String LOG_TAG = NewsActivity.class.getName();
     /**
-     * Constant value for the article loader ID. We can choose any integer.
+     * Constant value for the earthquake loader ID. We can choose any integer.
      * This really only comes into play if you're using multiple loaders.
      */
     private static final int NEWSARTICLE_LOADER_ID = 1;
@@ -32,6 +34,16 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
     public Loader<List<NewsArticle>> onCreateLoader(int i, Bundle bundle) {
         Log.i(LOG_TAG, "Test: onCreateLoader Called");
         // Create a new loader for the given URL
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // getString retrieves a String value from the preferences. The second parameter is the default value for this preference.
+        String minMagnitude = sharedPrefs.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+        String orderBy  = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
         return new NewsLoader(this, GUARDIAN_REQUEST_URL);
     }
     @Override
@@ -59,8 +71,8 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
         localAdapter.clear();
     }
     /** URL for article data from the Guardian news api */
-    //TODO: Insert grader URL and API key to test app, students key removed for security reasons
-    private static final String GUARDIAN_REQUEST_URL ="";
+    private static final String GUARDIAN_REQUEST_URL =
+            "https://content.guardianapis.com/search?q=housing&show-tags=contributor&api-key=88b7be55-1ef7-4d9d-81e5-a295fe565250&limit=10";
     private NewsArticleAdapter localAdapter;
 
     @Override
@@ -96,6 +108,13 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
                 startActivity(websiteIntent);
             }
         });
+        // Get a reference to the LoaderManager, in order to interact with loaders.
+        //LoaderManager loaderManager = getLoaderManager();
+
+        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+        // because this activity implements the LoaderCallbacks interface).
+        //loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
         // Get a reference to the ConnectivityManager to check state of network connectivity
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
